d8acbe916f526041bec5b8def69140bae58da8e2
v_mayonghua <v_mayonghua@baidudeMacBook-Pro-3.local>
2023-11-29 21:22:28 +0800

    vue3-base-frame 初版

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <RouterLink to="/">Home</RouterLink> | 
+  <RouterLink to="/about">About</RouterLink>
+  <hr>
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+
+</style>
