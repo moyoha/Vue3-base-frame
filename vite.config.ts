a88cabf0fdb3119ac0b4d673bada21dc513674a1
mayoha <mayoha@163.com>
2023-11-30 00:49:56 +0800

    build: 引入 unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,25 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/ // .vue
+      ],
+      // global imports to register
+      imports: [
+        'vue',
+        'vue-router',
+      ]
+    }),
   ],
   resolve: {
     alias: {
