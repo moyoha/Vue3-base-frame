95d9937a6904ffd55f582bb83c0575a7923932df
v_mayonghua <v_mayonghua@baidudeMacBook-Pro-3.local>
2023-11-30 20:01:09 +0800

    build: 引入 elementPlus

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 
 // https://vitejs.dev/config/
@@ -19,7 +21,11 @@ export default defineConfig({
       imports: [
         'vue',
         'vue-router',
-      ]
+      ],
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
     }),
   ],
   resolve: {
